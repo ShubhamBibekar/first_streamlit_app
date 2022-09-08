99a4c1493d219d5499c27c01caee69a40e4fe76c
Shubham Bibekar <113109042+ShubhamBibekar@users.noreply.github.com>
2022-09-08 18:46:39 +0530

    Update streamlit_app.py
    Added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
-import streamlit
-streamlit.title('My Parents New Healthy Diner At Bombay')
-streamlit.text('Let us Learn snowflake.')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
