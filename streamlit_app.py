9e4de208857f4019778a729d21af3b6e7471e8e8
Shubham Bibekar <113109042+ShubhamBibekar@users.noreply.github.com>
2022-09-11 16:31:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error 
-import URLerror
+from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
